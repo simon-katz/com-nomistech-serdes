42ff577872111e39e3811b91bcb768aa45662d5a
Simon Katz <sk@nomistech.com>
2021-07-18 22:42:30 +0100

    Initial commit from template

---
+++ test/com/nomistech/serdes_test.clj
@@ -0,0 +1,7 @@
+(ns com.nomistech.serdes-test
+  (:require [clojure.test :refer :all]
+            [com.nomistech.serdes :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
