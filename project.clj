8158c3dbe3ed6172d79fe454e6ad66f1d9de1794
Simon Katz <sk@nomistech.com>
2021-07-19 11:41:47 +0100

    Add URL to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject com.nomistech/serdes "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url         "https://github.com/simon-katz/com-nomistech-serdes"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
